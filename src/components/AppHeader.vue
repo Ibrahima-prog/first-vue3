8b9f2189257bbde468f0981a5693003409e1844b
root <root@DESKTOP-84SVONV>
2023-04-27 18:56:09 +0300

    done

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <head>
+            <link href="assets/output.css" rel="stylesheet">
+
+    <nav
+      class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
+    >
+      <a>Dc Heros</a>
+    </nav>
+</head>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style>
+  </style>
